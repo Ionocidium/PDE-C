7bbdd7195157c7b7cd79c5c46e63d2b0f6a9f15c
inyongthegr8 <inyong42@gmail.com>
2016-03-20 16:14:57 +0800

    CommandLineControls implementation
    Implemented all the required components to compile a program.
    
    Take note that cygwin is required to be able to compile the c files
    using gcc
    
    CommandLineControls - for 32/64 bit cygwin path
    CommandLineControls32 - for 32 bit cygwin path ONLY
    CommandLineControls64 - for 64 bit cygwin path ONLY

---
+++ src/controller/CommandLineControls.java
@@ -0,0 +1,93 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CommandLineControls
+{
+	private String gccPath;
+	private String fileToCompile;
+	private final String[] commands = {"", ""};
+    private Runtime rt = Runtime.getRuntime();
+    private Process proc;
+	private BufferedReader stdInput;
+	private BufferedReader stdError;
+
+	public CommandLineControls(String cFile) throws IOException
+	{
+		String path = "";
+		if(System.getProperty("os.arch").equals("amd64"))
+		{
+	        path = "C:\\cygwin64\\bin\\";
+		}
+		else if(System.getProperty("os.arch").equals("x86"))
+		{
+	        path = "C:\\cygwin\\bin\\";
+		}
+        this.gccPath = path;
+        this.fileToCompile = cFile;
+        this.rt = Runtime.getRuntime();
+        this.commands[0] = this.gccPath + "gcc.exe";
+        this.commands[1] = this.fileToCompile;
+	}
+	
+	public CommandLineControls(String gccPath, String cFile) throws IOException
+	{
+        this.gccPath = gccPath;
+        this.fileToCompile = cFile;
+        this.rt = Runtime.getRuntime();
+        this.commands[0] = this.gccPath + "gcc.exe";
+        this.commands[1] = this.fileToCompile;
+	}
+	
+	public String getStdOut() throws IOException{
+        this.proc = rt.exec(this.commands);
+        this.stdInput = new BufferedReader(new InputStreamReader(this.proc.getInputStream()));
+		String res = "";
+        String s = null;
+        while ((s = this.stdInput.readLine()) != null)
+        {
+            res += s + "\n";
+        }
+        return "Here is the standard output of the command:\n" + res;
+	}
+	
+	public String getStdError() throws IOException{
+        this.proc = rt.exec(this.commands);
+        this.stdError = new BufferedReader(new InputStreamReader(this.proc.getErrorStream()));
+		String res = "";
+        String s = null;
+        while ((s = this.stdError.readLine()) != null)
+        {
+            res += s + "\n";
+        }
+        return "Here is the standard error of the command:\n" + res;
+	}
+
+	public void sampler() throws IOException
+	{
+        Runtime rt = Runtime.getRuntime();
+        Process proc = rt.exec(this.commands);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null)
+        {
+            System.out.println(s);
+        }
+
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null)
+        {
+            System.out.println(s);
+        }
+    }
+	
+}
