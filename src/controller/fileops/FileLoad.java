0bc2b5467b358cfbdf26393ecd238dd20ead2386
Alex Jose <aljonjose@gmail.com>
2016-03-29 19:04:25 +0800

    File load
    File load implemented, not yet implemented in RSyntaxPane. I will
    integrate it later for the MainPage.

--- src/controller/fileops/FileLoad.java
+++ src/controller/fileops/FileLoad.java
@@ -1,15 +1,46 @@
 package controller.fileops;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class FileLoad {
-
-	public FileLoad(){
+public class FileLoad 
+{
+  public FileLoad()
+  {
 		
+  }
+
+  public String loadFile(Path path)
+  {
+	Charset charset = Charset.forName("US-ASCII");
+	String line = null;
+	String cCode = new String();
+	  
+	try (BufferedReader reader = Files.newBufferedReader(path, charset))
+	{		
+	  while ((line = reader.readLine()) != null)
+	  {
+		if (cCode.isEmpty())
+		{
+	      cCode += line;
+		}
+		  
+		else
+		{
+		  cCode = cCode + "\n" + line;
+		}	  
+	  }
 	}
-	
-	public String readMe(File path){
-		return "text content";
+	  
+	catch (IOException ex)
+	{
+	  ex.printStackTrace();
 	}
-	
+	  
+	return cCode;
+  }
 }
