b47f71c7ef6c5b7e3cc1bc0586c7f241eb4a2e25
Alex Jose <aljonjose@gmail.com>
2016-05-20 21:30:15 +0800

    Added parser file for parsing logic, no data yet

---
+++ src/service/Parsers.java
@@ -0,0 +1,20 @@
+package service;
+
+import java.net.URL;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
+import org.fife.ui.rsyntaxtextarea.parser.AbstractParser;
+import org.fife.ui.rsyntaxtextarea.parser.ParseResult;
+
+public class Parsers extends AbstractParser
+{
+
+  @Override
+  public ParseResult parse(RSyntaxDocument arg0, String arg1)
+  {
+	// TODO Auto-generated method stub
+	return null;
+  }
+
+  
+}
