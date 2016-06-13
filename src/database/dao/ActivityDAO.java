1d4468d6633f388525f88cf17a13c415d8fc61e0
inyongthegr8 <inyong42@gmail.com>
2016-06-13 08:46:13 +0800

    Working DAOs

--- src/database/dao/ActivityDAO.java
+++ src/database/dao/ActivityDAO.java
@@ -1,5 +1,58 @@
 package database.dao;
 
-public class ActivityDAO {
+import database.objects.Activity;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+public class ActivityDAO extends DAO{
+	
+	public void addActivity (Activity amdl) throws SQLException{
+        int IDNumber = amdl.getActivityID();
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Activity (ActivityID) values(?)");
+        preparedStatement.setInt(1, IDNumber);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public void deleteActivity (int activityID) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from Activity where ActivityID = ?");
+        preparedStatement.setInt(1, activityID);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public Activity getActivity (int activityID) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from Activity where ActivityID = ?");
+        preparedStatement.setInt(1, activityID);
+        ResultSet resultSet = query(preparedStatement);
+        Activity bmdl = new Activity();
+        while (resultSet.next()) {
+            int IDNumber = resultSet.getInt("ActivityID");
+            bmdl.setActivityID(IDNumber);
+        }
+        close(preparedStatement, connection);
+        return bmdl;
+    }
+    
+    public ArrayList<Activity> getActivities () throws SQLException{
+        ArrayList<Activity> activities = new ArrayList<Activity>();
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from Activity order by ActivityID");
+        ResultSet resultSet = query(preparedStatement);
+        while (resultSet.next()) {
+        	Activity amdl = new Activity();
+            int IDNumber = resultSet.getInt("IDnumber");
+            amdl.setActivityID(IDNumber);
+            activities.add(amdl);
+        }
+        close(preparedStatement, connection);
+        return activities;
+    }
+    
 }
--- src/database/dao/DeliverableDAO.java
+++ src/database/dao/DeliverableDAO.java
@@ -1,5 +1,58 @@
 package database.dao;
 
-public class DeliverableDAO {
+import database.objects.Deliverable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DeliverableDAO extends DAO{
+	
+	public void addDeliverable (Deliverable dmdl) throws SQLException{
+        int IDNumber = dmdl.getDeliverableID();
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Deliverable (DeliverableID) values(?)");
+        preparedStatement.setInt(1, IDNumber);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public void deleteDeliverable (int deliverableID) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from Deliverable where DeliverableID = ?");
+        preparedStatement.setInt(1, deliverableID);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public Deliverable getDeliverable (int deliverableID) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from Deliverable where DeliverableID = ?");
+        preparedStatement.setInt(1, deliverableID);
+        ResultSet resultSet = query(preparedStatement);
+        Deliverable bmdl = new Deliverable();
+        while (resultSet.next()) {
+            int IDNumber = resultSet.getInt("DeliverableID");
+            bmdl.setDeliverableID(IDNumber);
+        }
+        close(preparedStatement, connection);
+        return bmdl;
+    }
+    
+    public ArrayList<Deliverable> getDeliverables () throws SQLException{
+        ArrayList<Deliverable> activities = new ArrayList<Deliverable>();
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from Deliverable order by DeliverableID");
+        ResultSet resultSet = query(preparedStatement);
+        while (resultSet.next()) {
+        	Deliverable dmdl = new Deliverable();
+            int IDNumber = resultSet.getInt("IDnumber");
+            dmdl.setDeliverableID(IDNumber);
+            activities.add(dmdl);
+        }
+        close(preparedStatement, connection);
+        return activities;
+    }
 
 }
