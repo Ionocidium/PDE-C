ba8b0faccdd36cf67fff8c910f6fd513439de907
inyongthegr8 <inyong42@gmail.com>
2016-06-15 22:14:38 +0800

    Completed all DAOs
    Detailed changes were made.
    
    Passing values whenever possible. To be discussed on Friday.

--- src/database/dao/ActivityDAO.java
+++ src/database/dao/ActivityDAO.java
@@ -1,19 +1,38 @@
 package database.dao;
 
 import database.objects.Activity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.sql.Date;
 
 public class ActivityDAO extends DAO{
 	
-	public void addActivity (Activity amdl) throws SQLException{
-        int IDNumber = amdl.getActivityID();
+	public void addActivity (Activity amdl) throws SQLException, FileNotFoundException{
+        int activityID = amdl.getActivityID();
+        String activityName = amdl.getActivityName();
+    	File activityFile = amdl.getActivityFile();
+    	Timestamp activityTimeStamp = amdl.getActivityTimeStamp();
+    	Date activityDeadline = amdl.getActivityDeadline();
+    	String activityFilename = amdl.getActivityFilename();
         Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into Activity (ActivityID) values(?)");
-        preparedStatement.setInt(1, IDNumber);
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Activity (ActivityID, ActivityName, ActivityFile, ActivityTimestamp, ActivityDeadline, ActivityFilename) values(?)");
+        preparedStatement.setInt(1, activityID);
+        preparedStatement.setString(2, activityName);
+        preparedStatement.setBlob(3, new FileInputStream(activityFile));
+        preparedStatement.setTimestamp(4, activityTimeStamp);
+        preparedStatement.setDate(5, activityDeadline);
+        preparedStatement.setString(6, activityFilename);
         update(preparedStatement);
         close(preparedStatement, connection);
     }
@@ -26,29 +45,88 @@ public class ActivityDAO extends DAO{
         close(preparedStatement, connection);
     }
     
-    public Activity getActivity (int activityID) throws SQLException{
+    public void changeDate (int activityID, Date newDate) throws SQLException{
         Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from Activity where ActivityID = ?");
-        preparedStatement.setInt(1, activityID);
-        ResultSet resultSet = query(preparedStatement);
-        Activity bmdl = new Activity();
-        while (resultSet.next()) {
-            int IDNumber = resultSet.getInt("ActivityID");
-            bmdl.setActivityID(IDNumber);
-        }
+        PreparedStatement preparedStatement = connection.prepareStatement("update Activity set ActivityDeadline = ?, ActivityTimestamp = ? where ActivityID = ?");
+        preparedStatement.setDate(1, newDate);
+        preparedStatement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+        preparedStatement.setInt(3, activityID);
+        update(preparedStatement);
         close(preparedStatement, connection);
-        return bmdl;
     }
     
-    public ArrayList<Activity> getActivities () throws SQLException{
+    public void changeName (int activityID, String newName) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update Activity set ActivityName = ?, ActivityTimestamp = ? where ActivityID = ?");
+        preparedStatement.setString(1, newName);
+        preparedStatement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+        preparedStatement.setInt(3, activityID);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public void updateFile (int activityID, File newFile) throws SQLException, FileNotFoundException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update Activity set ActivityFile = ?, ActivityFileName = ?, ActivityTimestamp = ? where ActivityID = ?");
+        preparedStatement.setBlob(1, new FileInputStream(newFile));
+        preparedStatement.setString(2, newFile.getName());
+        preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+        preparedStatement.setInt(4, activityID);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+    
+    public Activity getActivity (int idNumber) throws SQLException, IOException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from Activity where ActivityID = ?");
+        preparedStatement.setInt(1, idNumber);
+        ResultSet resultSet = query(preparedStatement);
+        Activity amdl = new Activity();
+        while (resultSet.next()) {
+            int activityID = resultSet.getInt("ActivityID");
+            String activityName = resultSet.getString("ActivityName");
+            String activityFilename = resultSet.getString("ActivityFilename");
+        	Blob b = resultSet.getBlob("ActivityFile");
+        	InputStream binaryStream = b.getBinaryStream(0, b.length());
+        	byte[] buffer = new byte[binaryStream.available()];
+            binaryStream.read(buffer);
+        	File activityFile = new File(activityFilename);
+            Timestamp activityTimeStamp = resultSet.getTimestamp("ActivityTimeStamp");
+            Date activityDeadline = resultSet.getDate("ActivityDeadline");
+            amdl.setActivityID(activityID);
+            amdl.setActivityName(activityName);
+            amdl.setActivityFile(activityFile);
+            amdl.setActivityTimeStamp(activityTimeStamp);
+            amdl.setActivityDeadline(activityDeadline);
+            amdl.setActivityFilename(activityFilename);
+        }
+        close(preparedStatement, connection);
+        return amdl;
+    }
+    
+    public ArrayList<Activity> getActivities () throws SQLException, IOException{
         ArrayList<Activity> activities = new ArrayList<Activity>();
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from Activity order by ActivityID");
         ResultSet resultSet = query(preparedStatement);
         while (resultSet.next()) {
         	Activity amdl = new Activity();
-            int IDNumber = resultSet.getInt("IDnumber");
-            amdl.setActivityID(IDNumber);
+            int activityID = resultSet.getInt("ActivityID");
+            String activityName = resultSet.getString("ActivityName");
+            String activityFilename = resultSet.getString("ActivityFilename");
+        	Blob b = resultSet.getBlob("ActivityFile");
+        	InputStream binaryStream = b.getBinaryStream(0, b.length());
+        	byte[] buffer = new byte[binaryStream.available()];
+            binaryStream.read(buffer);
+        	File activityFile = new File(activityFilename);
+            Timestamp activityTimeStamp = resultSet.getTimestamp("ActivityTimeStamp");
+            Date activityDeadline = resultSet.getDate("ActivityDeadline");
+            amdl.setActivityID(activityID);
+            amdl.setActivityName(activityName);
+            amdl.setActivityFile(activityFile);
+            amdl.setActivityTimeStamp(activityTimeStamp);
+            amdl.setActivityDeadline(activityDeadline);
+            amdl.setActivityFilename(activityFilename);
             activities.add(amdl);
         }
         close(preparedStatement, connection);
--- src/database/dao/DeliverableDAO.java
+++ src/database/dao/DeliverableDAO.java
@@ -1,19 +1,63 @@
 package database.dao;
 
 import database.objects.Deliverable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class DeliverableDAO extends DAO{
 	
-	public void addDeliverable (Deliverable dmdl) throws SQLException{
-        int IDNumber = dmdl.getDeliverableID();
+	public void addDeliverable (Deliverable dmdl) throws SQLException, FileNotFoundException{
+        int deliverableID = dmdl.getDeliverableID();
+    	int studentID = dmdl.getStudentID();
+    	int activityID = dmdl.getActivityID();
+    	File deliverableSourceCode = dmdl.getDeliverableSourceCode();
+    	Timestamp dateSubmitted = dmdl.getDateSubmitted();
+    	String deliverableSourceCodeFileName = dmdl.getDeliverableSourceCodeFileName();
+    	float grade = dmdl.getGrade();
         Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into Deliverable (DeliverableID) values(?)");
-        preparedStatement.setInt(1, IDNumber);
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Deliverable (DeliverableID, StudentID, ActivityID, DeliverableSourceCode, DateSubmitted, DeliverableSourceCodeFileName, Grade) values(?, ?, ?, ?, ?, ?, ?)");
+        preparedStatement.setInt(1, deliverableID);
+        preparedStatement.setInt(2, studentID);
+        preparedStatement.setInt(3, activityID);
+        preparedStatement.setBlob(4, new FileInputStream(deliverableSourceCode));
+        preparedStatement.setTimestamp(5, dateSubmitted);
+        preparedStatement.setString(6, deliverableSourceCodeFileName);
+        preparedStatement.setFloat(7, grade);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+	
+	public void changeGrade (int studentID, int activityID, Float newGrade) throws SQLException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update Deliverable set Grade = ? where StudentID = ? AND ActivityID = ?");
+        preparedStatement.setFloat(1, newGrade);
+        preparedStatement.setInt(2, studentID);
+        preparedStatement.setInt(3, activityID);
+        update(preparedStatement);
+        close(preparedStatement, connection);
+    }
+	
+	public void uploadNewSubmission (int studentID, int activityID, File newFile) throws SQLException, FileNotFoundException{
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update Deliverable set DeliverableSourceCode = ?, DeliverableSourceCodeFileName = ?, DateSubmitted = ? where StudentID = ? AND ActivityID = ?");
+        preparedStatement.setBlob(1, new FileInputStream(newFile));
+        preparedStatement.setString(2, newFile.getName());
+        preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+        preparedStatement.setInt(4, studentID);
+        preparedStatement.setInt(5, activityID);
         update(preparedStatement);
         close(preparedStatement, connection);
     }
@@ -26,29 +70,68 @@ public class DeliverableDAO extends DAO{
         close(preparedStatement, connection);
     }
     
-    public Deliverable getDeliverable (int deliverableID) throws SQLException{
+    public Deliverable getDeliverable (int idNumber) throws SQLException, IOException{
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from Deliverable where DeliverableID = ?");
-        preparedStatement.setInt(1, deliverableID);
+        preparedStatement.setInt(1, idNumber);
         ResultSet resultSet = query(preparedStatement);
-        Deliverable bmdl = new Deliverable();
+        Deliverable dmdl = new Deliverable();
         while (resultSet.next()) {
-            int IDNumber = resultSet.getInt("DeliverableID");
-            bmdl.setDeliverableID(IDNumber);
+            int deliverableID = resultSet.getInt("DeliverableID");
+        	int studentID = resultSet.getInt("StudentID");
+        	int activityID = resultSet.getInt("ActivityID");
+        	String deliverableSourceCodeFileName = resultSet.getString("DeliverableSourceCodeFileName");
+        	Blob b = resultSet.getBlob("DeliverableSourceCode");
+        	InputStream binaryStream = b.getBinaryStream(0, b.length());
+        	byte[] buffer = new byte[binaryStream.available()];
+            binaryStream.read(buffer);
+        	File deliverableSourceCode = new File(deliverableSourceCodeFileName);
+        	OutputStream outStream = new FileOutputStream(deliverableSourceCode);
+        	outStream.write(buffer);
+        	outStream.close();
+        	Timestamp dateSubmitted = resultSet.getTimestamp("DateSubmitted");
+        	float grade = resultSet.getFloat("Grade");
+            dmdl.setDeliverableID(deliverableID);
+            dmdl.setStudentID(studentID);
+            dmdl.setActivityID(activityID);
+            dmdl.setDeliverableSourceCode(deliverableSourceCode);
+            dmdl.setDateSubmitted(dateSubmitted);
+            dmdl.setDeliverableSourceCodeFileName(deliverableSourceCodeFileName);
+            dmdl.setGrade(grade);
         }
         close(preparedStatement, connection);
-        return bmdl;
+        return dmdl;
     }
     
-    public ArrayList<Deliverable> getDeliverables () throws SQLException{
+    public ArrayList<Deliverable> getDeliverables () throws SQLException, IOException{
         ArrayList<Deliverable> activities = new ArrayList<Deliverable>();
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from Deliverable order by DeliverableID");
         ResultSet resultSet = query(preparedStatement);
         while (resultSet.next()) {
         	Deliverable dmdl = new Deliverable();
-            int IDNumber = resultSet.getInt("IDnumber");
-            dmdl.setDeliverableID(IDNumber);
+        	int deliverableID = resultSet.getInt("DeliverableID");
+        	int studentID = resultSet.getInt("StudentID");
+        	int activityID = resultSet.getInt("ActivityID");
+        	String deliverableSourceCodeFileName = resultSet.getString("DeliverableSourceCodeFileName");
+        	Blob b = resultSet.getBlob("DeliverableSourceCode");
+        	InputStream binaryStream = b.getBinaryStream(0, b.length());
+        	byte[] buffer = new byte[binaryStream.available()];
+            binaryStream.read(buffer);
+        	File deliverableSourceCode = new File(deliverableSourceCodeFileName);
+        	OutputStream outStream = new FileOutputStream(deliverableSourceCode);
+        	outStream.write(buffer);
+        	outStream.close();
+        	Timestamp dateSubmitted = resultSet.getTimestamp("DateSubmitted");
+        	float grade = resultSet.getFloat("Grade");
+            dmdl.setDeliverableID(deliverableID);
+            dmdl.setStudentID(studentID);
+            dmdl.setActivityID(activityID);
+            dmdl.setDeliverableSourceCode(deliverableSourceCode);
+            dmdl.setDateSubmitted(dateSubmitted);
+            dmdl.setDeliverableSourceCodeFileName(deliverableSourceCodeFileName);
+            dmdl.setGrade(grade);
+            dmdl.setDeliverableID(deliverableID);
             activities.add(dmdl);
         }
         close(preparedStatement, connection);
