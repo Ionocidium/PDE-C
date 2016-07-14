2e5c2ce6dfc18f5cbda4865667aa70bd1bbdd946
inyongthegr8 <inyong42@gmail.com>
2016-07-14 12:11:09 +0800

    Transferred CBR-C to the server.

--- src/view/CompileLog.java
+++ src/view/CompileLog.java
@@ -63,12 +63,12 @@ public class CompileLog {
 		frmCompileLog.setLocationRelativeTo(null);
 		frmCompileLog.setVisible(true);
 		CommandLineControls clc = new CommandLineControls(cPath.toString());
-		CBRCIntegration cbrc = new CBRCIntegration();
+		//CBRCIntegration cbrc = new CBRCIntegration();
 		
 		JTextPane txtpnTest = new JTextPane();
 		txtpnTest.setEditable(false);
 		txtpnTest.setFont(new Font("Monospaced", Font.PLAIN, 12));
-		txtpnTest.setText(clc.getStdOut() + "\n" + clc.getStdError() + "\n" + cbrc.feedback0());
+		//txtpnTest.setText(clc.getStdOut() + "\n" + clc.getStdError() + "\n" + cbrc.feedback0());
 		txtpnTest.setCaretPosition(0);
 		// frmCompileLog.getContentPane().add(txtpnTest, BorderLayout.CENTER);
 		
