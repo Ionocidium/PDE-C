a42d2922e47d46859f58e416a2ffd4f713154392
Alex Jose <aljonjose@gmail.com>
2016-11-03 11:04:29 +0800

    Added the compile function and compile & run

--- src/api/component/PCompiler.java
+++ src/api/component/PCompiler.java
@@ -1,14 +1,22 @@
 package api.component;
 
+import java.nio.file.Path;
+
 import configuration.LocalConfiguration;
+import controller.EventController;
+import view.MainWindowView;
 
 public class PCompiler
 {
   private LocalConfiguration config;
+  private MainWindowView main;
+  private EventController event;
   
   public PCompiler()
   {
 	config = LocalConfiguration.getInstance();
+	main = MainWindowView.getInstance();
+	event = EventController.getEventController();
   }
   
   public void setCompiler(String path)
@@ -19,5 +27,26 @@ public class PCompiler
   public String getCurrentCompiler()
   {
 	return config.getGccPath();
+  }
+  
+  public Path compile(Path filePath)
+  {
+	return event.compile(main.getMainFrame(), main.getEditor(), filePath, main.getErrorLog());
+  }
+  
+  public void compileRun(Path filePath)
+  {
+	Path next = compile(filePath);
+	
+	if (!main.getErrorLog().getText().trim().equals(""))
+	{
+	  
+	}
+	
+	else
+	{
+	  event.runProgram(next);	  
+	}
+
   }
 }
