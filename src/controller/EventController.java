f8ed551c3fc295e4c3d49b7654db7bd18eae0f43
Alex Jose <aljonjose@gmail.com>
2016-11-21 22:32:51 +0800

    Error dialog for gcc not found

--- src/controller/EventController.java
+++ src/controller/EventController.java
@@ -277,6 +277,9 @@ public class EventController
 	{
 	  
 	  Path path = null;
+	  
+	  if (this.checkIfGccExists())
+	  {
 		try
 		{
 			if (filePath != null)
@@ -349,6 +352,13 @@ public class EventController
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+	  }
+	  
+	  else
+	  {
+		JOptionPane.showMessageDialog(null, "GCC not found", "Error", JOptionPane.ERROR_MESSAGE);
+	  }
+
 		
 		return path;
 	}
@@ -953,5 +963,19 @@ public class EventController
 		{
 			long nowTime = System.currentTimeMillis();
 			while(System.currentTimeMillis() < nowTime + ms) ;
+		}
+		
+		private boolean checkIfGccExists()
+		{
+		  boolean itExists = false;
+		  
+		  LocalConfiguration local = LocalConfiguration.getInstance();
+		  
+		  if (Files.exists(Paths.get(local.getGccPath())))
+		  {
+			itExists = true;
+		  }
+		  
+		  return itExists;
 		}
 	}
