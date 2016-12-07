504061daa54ec04523ad37204cc3814c823824f6
Alex Jose <aljonjose@gmail.com>
2016-12-07 23:16:06 +0800

    Added documentation for PButton and PButtonGroup

--- src/api/component/PButton.java
+++ src/api/component/PButton.java
@@ -10,6 +10,15 @@ import buttonactions.SendAction;
 import mainwindowcomponents.ToolBar;
 import view.MainWindowView;
 
+/**
+ * A PDE-C specific button class that extends the JButton class. It contains all of
+   JButton methods. In addition, some methods are also added by the researchers
+   for the ease of use of other developers.
+ * 
+ * @author Alexander John D. Jose
+ *
+ */
+
 public class PButton extends JButton
 {
   public static final int DOWNLOAD_ACTION = 0;
@@ -21,12 +30,20 @@ public class PButton extends JButton
   private ToolBar bar = ToolBar.getToolbar();
   private MainWindowView main = MainWindowView.getInstance();
   
+  /**
+   * 
+   * @param the desired name of PButton
+   */
   public PButton(String name)
   {
 	bName = name;
 	this.setText(bName);
   }
   
+  /**
+   * The main function of this
+	method is to add the PButton to PDE-C’s toolbar.
+   */
   public void addToToolbar()
   {
 	if (this != null)
@@ -35,11 +52,39 @@ public class PButton extends JButton
 	}	
   }
   
+  /**
+   * A method that remove the said component from PDE-C. This
+	 does not need any parameter since it instantly remove
+	 the component.
+   */
   public void remove()
   {
 	bar.removeComponent(this);
   }
   
+  /**
+   * This method adds a pre-made action to PButton.
+   * @param action Integer value that determines if the buttons is to be
+                   made to a download button, send source code button, or
+				   compile button. pecific codes are:
+				   PButton.DOWNLOAD ACTION,
+				   PButton.SEND ACTION,
+	               PButton.COMPILE ACTION
+				   
+   * @param filePath This is an optional parameter for send and compile action. If
+					 this parameter is not null, the button will send or compile a file
+					 that is passed inside the filePath.
+					 Download action should
+					 always have a null value for this
+
+   * @param code This is a required parameter that must be with the Compile
+				 action input. The specific codes are:
+			 	 CompileAction.COMPILE,
+				 CompileAction.COMPILE RUN,
+				 CompileAction.NO ACTION.
+				 No Action code must be used when the button is not to be
+				 programmed as a compile button.
+   */
   public void addAction(int action, Path filePath, int code)
   {
 	switch (action)
--- src/api/component/PButtonGroup.java
+++ src/api/component/PButtonGroup.java
@@ -3,24 +3,42 @@ package api.component;
 import java.util.ArrayList;
 
 import mainwindowcomponents.ToolBar;
-
+/**
+ * Groups the buttons before adding it to PDE-C’s
+   toolbar.
+ * 
+ * @author Alexander John D. Jose
+ *
+ */
 public class PButtonGroup
 {
   
   private ArrayList<PButton> button;
   private ToolBar bar;
   
+  /**
+   * 
+   */
   public PButtonGroup()
   {
 	button = new ArrayList<PButton>();
 	bar = ToolBar.getToolbar();
   }
   
+  /**
+   * Adds a button to the group.
+   * @param button this is a PButton that will be added to the PButtonGroup.
+   */
   public void addButton(PButton button)
   {
 	this.button.add(button);
   }
   
+  /**
+   * This method is used after adding button
+ 	 objects to the ButtonGroup. It builds the
+	 group and add it to PDE-C’s toolbar.
+   */
   public void build()
   {
 	bar.addSeparator();
