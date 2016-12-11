8a6c005b0013806db1bbf163605c59a437b01c61
inyongthegr8 <inyong42@gmail.com>
2016-12-11 20:54:09 +0800

    ErrorMessage and Feedback

--- src/model/ErrorMessage.java
+++ src/model/ErrorMessage.java
@@ -1,41 +1,77 @@
 package model;
-
+/**
+ * Handles the encapsulation and representation of <code>ErrorMessage</code> model.
+ * 
+ * @author Lorenzo Miguel G. Monzon
+ */
 public class ErrorMessage {
 
 	private String type;
 	private String message;
 	private String line;
 	
+	/**
+	 * Creates a representation that contains the information for <code>ErrorMessage</code> model.
+	 * @param type the type of error
+	 * @param message the message contained in the error
+	 * @param line the line number of the error message 
+	 */
 	public ErrorMessage(String type, String message, String line) {
 		this.type = type;
 		this.message = message;
 		this.line = line;
 	}
 	
+	/**
+	 * Gets the <code>type</code> property.
+	 * @return the <code>type</code>
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Sets the <code>type</code> to its preferred value.
+	 * @param type the <code>type</code> to set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
-
+	/**
+	 * Gets the <code>message</code> property.
+	 * @return the <code>message</code>
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * Sets the <code>message</code> to its preferred value.
+	 * @param message the <code>message</code> to set
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
+	/**
+	 * Gets the <code>line</code> property.
+	 * @return the <code>line</code>
+	 */
 	public String getLine() {
 		return line;
 	}
 
+	/**
+	 * Sets the <code>line</code> to its preferred value.
+	 * @param line the <code>line</code> to set
+	 */
 	public void setLine(String line) {
 		this.line = line;
 	}
 	
+	/**
+	 * Gets the error message of the said line.
+	 * @return the error message of the said line
+	 */
 	public String getErrorMessage() {
 		return this.getType() + this.getMessage() + this.getLine();
 	}
--- src/model/Feedback.java
+++ src/model/Feedback.java
@@ -1,27 +1,53 @@
 package model;
 
+/**
+ * Handles the encapsulation and representation of <code>Feedback</code> model.
+ * 
+ * @author Lorenzo Miguel G. Monzon
+ */
 public class Feedback {
 
 	private String error;
 	private String code;
 	
+	/**
+	 * Creates a representation that contains the information for <code>Feedback</code> model.
+	 * @param error the error message
+	 * @param code the source code
+	 */
 	public Feedback(String error, String code) {
 		this.error = error;
 		this.code = code;
 	}
-	
+
+	/**
+	 * Gets the <code>error</code> property.
+	 * @return the <code>error</code>
+	 */
 	public String getError() {
 		return error;
 	}
 
+	/**
+	 * Sets the <code>error</code> to its preferred value.
+	 * @param error the <code>error</code> to set
+	 */
 	public void setError(String error) {
 		this.error = error;
 	}
 
+	/**
+	 * Gets the <code>code</code> property.
+	 * @return the <code>code</code>
+	 */
 	public String getCode() {
 		return code;
 	}
 
+	/**
+	 * Sets the <code>code</code> to its preferred value.
+	 * @param code the <code>code</code> to set
+	 */
 	public void setCode(String code) {
 		this.code = code;
 	}
