1414718c59f8a3faa8b41779176bf63b65dba8b3
Milo Monzon <monzonmilo@yahoo.com>
2016-12-15 00:13:10 +0800

    Added Documentation

--- src/api/component/PDialogBox.java
+++ src/api/component/PDialogBox.java
@@ -2,12 +2,24 @@ package api.component;
 
 import javax.swing.JOptionPane;
 /**
+ * This class uses JOptionPane to add dialog boxes in PDE-C.
  * 
  * @author Lorenzo Miguel Monzon
  *
  */
 public class PDialogBox{
 
+	/** 
+	 * <p>
+	 * There are four types of dialog boxes in PDE-C
+	 * <ul>
+	 *  <li>INFORMATION - A dialog box that only contains a message</li>
+	 *  <li>WARNING - A dialog box that contains a title, message and a warning symbol</li>
+	 *  <li>ERROR - A dialog box that contains a title, message and an error symbol</li>
+	 *  <li>SIMPLE - A dialog box that contains a title and message</li>
+	 *  </ul>
+	 * </p>
+	 */
 	public static final int INFORMATION = 0;
 	public static final int WARNING = 1;
 	public static final int ERROR = 2;
@@ -17,6 +29,12 @@ public class PDialogBox{
 	private String message;
 	private int type;
 	
+	  /**
+	   * The constructor of PDialogBox
+	   * @param title this is the name of the dialog box
+	   * @param message this is the message of the dialog box
+	   * @param MESSAGE_TYPE this is the type of dialog box. Refer to default values for each message type.
+	   */
 	public PDialogBox(String title, String message, int MESSAGE_TYPE)
 	{
 		this.title = title;
@@ -24,6 +42,9 @@ public class PDialogBox{
 		this.type = MESSAGE_TYPE;
 	}
 	
+	/**
+	 * This method shows the dialog box in PDE-C
+	 */
 	public void showMessage()
 	{
 		switch (type)
--- src/api/component/POptionBox.java
+++ src/api/component/POptionBox.java
@@ -1,18 +1,33 @@
 package api.component;
 
 import javax.swing.JOptionPane;
+/**
+ * This class uses JOptionPane to add option boxes in PDE-C. The option box will always have a Yes or No option.
+ * 
+ * @author Lorenzo Miguel Monzon
+ *
+ */
 
 public class POptionBox {
 	
 	private String title;
 	private String message;
 	
+	  /**
+	   * The constructor of PDialogBox
+	   * @param title this is the name of the option box
+	   * @param message this is the message of the option box
+	   */
 	public POptionBox(String title, String message)
 	{
 		this.title = title;
 		this.message = message;
 	}
 	
+	/**
+	 * This method shows the option box in PDE-C
+	 * @return the integer value (0 for No, 1 for Yes) of the selected option
+	 */
 	public int showMessage()
 	{
 		return JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
